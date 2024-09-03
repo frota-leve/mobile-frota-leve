f8a41bfe5643920928b328bc0ed211ea53b03bea
Davi Taveira <devtaveira@gmail.com>
2024-09-02 23:30:56 -0300

    login screen

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,5 +1,13 @@
 import React, { Dispatch, SetStateAction } from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import {
+  Button,
+  Image,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import Input from "../components/Input";
 
 const Home = ({
   setSession,
@@ -7,14 +15,16 @@ const Home = ({
   setSession: Dispatch<SetStateAction<boolean>>;
 }) => {
   return (
-    <View className="bg-gray-500 h-full flex justify-center items-center">
-      <View className=" rounded-md p-5 bg-white">
-        <TouchableOpacity
-          className="rounded-lg bg-blue-500 text-black"
-          onPress={() => setSession(false)}
-        >
-          <Text>Home</Text>
-        </TouchableOpacity>
+    <View className="bg-white h-full justify-center items-center">
+      <View className="w-[80%] justify-center items-center h-full">
+        <View className="items-center rounded-xl w-full py-2">
+          <TouchableOpacity
+            className="w-full items-center justify-center rounded-xl mt-8 h-14 bg-[#FFC314] text-black"
+            onPress={() => setSession(false)}
+          >
+            <Text className="font-semibold text-2xl">Sair</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
--- pages/Login.tsx
+++ pages/Login.tsx
@@ -1,5 +1,12 @@
 import React, { Dispatch, SetStateAction } from "react";
-import { Button, Text, TextInput, TouchableOpacity, View } from "react-native";
+import {
+  Button,
+  Image,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import Input from "../components/Input";
 
 const Login = ({
@@ -8,16 +15,34 @@ const Login = ({
   setSession: Dispatch<SetStateAction<boolean>>;
 }) => {
   return (
-    <View className="bg-blue-500 h-full flex justify-center items-center">
-      <View className="flex gap-4 rounded-md p-5 bg-black">
-        <Input placeholder="Email" />
-        <Input placeholder="Password" />
-        <TouchableOpacity
-          className="rounded-lg bg-red-500 text-black"
-          onPress={() => setSession(true)}
-        >
-          <Text>Login</Text>
-        </TouchableOpacity>
+    <View className="bg-white h-full justify-center items-center">
+      <View className="w-[80%] justify-center items-center h-full">
+        <Image
+          resizeMode="contain"
+          className="w-full"
+          source={require("../public/logo-big.png")}
+        />
+        <View className="items-center rounded-xl w-full py-2">
+          <Text className="text-lg items-start mt-2 w-full font-semibold">
+            Email
+          </Text>
+          <Input />
+          <Text className="text-lg items-start mt-2 w-full font-semibold">
+            Senha
+          </Text>
+          <Input />
+          <View className="w-full items-end">
+            <Text className="color-slate-400 text-sm font-semibold">
+              Esqueceu?
+            </Text>
+          </View>
+          <TouchableOpacity
+            className="w-full items-center justify-center rounded-xl mt-8 h-14 bg-[#FFC314] text-black"
+            onPress={() => setSession(true)}
+          >
+            <Text className="font-semibold text-2xl">Entrar</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
